f8195addbde583a5ce633a39ca97274d7ac6921a
nuhmanparamban <nuhmanp@gmail.com>
2024-02-23 09:27:38 -0500

    stubbed postman service to execute post calls

--- postman-collection-backend/src/main/java/com/nuhman/postman/service/impl/PostmanServiceImpl.java
+++ postman-collection-backend/src/main/java/com/nuhman/postman/service/impl/PostmanServiceImpl.java
@@ -1,6 +1,35 @@
 package com.nuhman.postman.service.impl;
 
+import com.nuhman.postman.model.PostmanWrapper;
 import com.nuhman.postman.service.PostmanService;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class PostmanServiceImpl implements PostmanService {
+    @Override
+    public void processRequest(PostmanWrapper postmanWrapper) {
+
+    }
+
+
+    public static void httpPostRequest() throws URISyntaxException, IOException, InterruptedException {
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+        HttpRequest request = HttpRequest.newBuilder(new URI("http://jsonplaceholder.typicode.com/posts"))
+                .version(HttpClient.Version.HTTP_2)
+                .POST(HttpRequest.BodyPublishers.ofString("Sample Post Request"))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String responseBody = response.body();
+        System.out.println("httpPostRequest : " + responseBody);
+    }
+
+    
+
 }
